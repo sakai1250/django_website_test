43011526751b88260bdcf5d90a3ddb3a2f4e030b
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-10-24 23:31:28 +0900

    add:photo and def get_context_data

--- django10:24/django_website2/website/views.py
+++ django10:24/django_website2/website/views.py
@@ -3,6 +3,10 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "index.html"
     
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        return ctxt
+    
 class AboutView(TemplateView):
     template_name = "about.html"
 
